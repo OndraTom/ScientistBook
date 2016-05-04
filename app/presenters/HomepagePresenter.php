d1da2c484fb03e20ba5e99779f4c9245a493b0f0
Ondřej Tom <info@ondratom.cz>
2016-05-04 21:08:31 +0200

    Front homepage redirection.

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -4,4 +4,10 @@ namespace App\Presenters;
 
 class HomepagePresenter extends BasePresenter
 {
+	public function startup() 
+	{
+		parent::startup();
+		
+		$this->redirect(':Front:Homepage:default');
+	}
 }
